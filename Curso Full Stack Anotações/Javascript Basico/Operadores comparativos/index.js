38d0f126030685571304a803d4c350e2d320d367
Thaynansp29 <thaynansp29@hotmail.com>
2022-10-31 19:45:29 -0300

    31102022

---
+++ Curso Full Stack Anotações/Javascript Basico/Operadores comparativos/index.js
@@ -0,0 +1,19 @@
+// == igual a
+// === valor igual e igual
+// ! = não é igual
+// !== não igual ou não igual
+// > maior que
+// < menor de
+// > = maior que ou igual a
+// <= menor ou igual a
+
+var a = 4;
+var b = 5;
+
+console.log(a == b);
+console.log(a === b);
+console.log(a != b);
+console.log(a !== b);
+console.log(a > b);
+console.log(a < b);
+console.log(a <= b);
