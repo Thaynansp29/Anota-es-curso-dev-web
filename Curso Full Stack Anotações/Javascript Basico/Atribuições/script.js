fc30a82cd17ab88a85c9447c6ded98891042c140
Thaynansp29 <thaynansp29@hotmail.com>
2022-10-31 18:13:01 -0300

    31102022

---
+++ Curso Full Stack Anotações/Javascript Basico/Atribuições/script.js
@@ -0,0 +1,23 @@
+// = x = y  x = y
+// += x += y x = x + y
+// -= x -= y x = x - y
+// *= x *= y x = x * y
+// /= x /= y x = x / y
+// %= x %= y x = x % y
+
+var a = 30;
+var b = 30;
+var c = 30;
+var d = 30;
+var e = 30;
+
+a += 2;
+b -= 2;
+c *= 2;
+d /= 2;
+e %= 2;
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
