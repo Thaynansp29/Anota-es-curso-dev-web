b0903505d943e72e622bcfb56fa9273498377281
Thaynansp29 <thaynansp29@hotmail.com>
2022-11-01 19:31:10 -0300

    01112022

---
+++ Curso Full Stack Anotações/Javascript Basico/Strings/script.js
@@ -0,0 +1,5 @@
+var numero = "88" * 2;
+var str = "Valor qualquer" * 2;
+
+console.log(numero);
+console.log(str);
---
+++ Curso Full Stack Anotações/Javascript Basico/Transformando Strings em Números/script.js
@@ -0,0 +1,7 @@
+var a = "3.5";
+
+var b = "6";
+
+var c = Number(a) + Number(b);
+
+console.log(c);
