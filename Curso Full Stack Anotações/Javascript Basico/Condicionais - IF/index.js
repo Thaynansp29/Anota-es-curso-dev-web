fc2cb3d546e3cfce6a04c593c916d74a67a70054
Thaynansp29 <thaynansp29@hotmail.com>
2022-11-01 20:53:34 -0300

    01112022

---
+++ Curso Full Stack Anotações/Javascript Basico/Condicionais - IF/index.js
@@ -0,0 +1,7 @@
+var idade = Number(prompt("Quantos anos você tem?"));
+if (idade >= 18 || idade <= 70) {
+  alert(`Seja bem vindo ao nosso site`);
+  alert(`Mostre a identidade`);
+} else {
+  alert(`Infelizmente você não pode entrar`);
+}
