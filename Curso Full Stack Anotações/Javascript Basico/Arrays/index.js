ba7ac8fe343a62c7d21ed02a59ec955b8926f03c
Thaynansp29 <thaynansp29@hotmail.com>
2022-11-08 16:46:07 -0300

    08112022

---
+++ Curso Full Stack Anotações/Javascript Basico/Arrays/index.js
@@ -0,0 +1,12 @@
+alunos = ["Igor", "José", "Maria", "Fernanda"];
+notas1 = [6.5, 5.5, 3, 7.8];
+notas2 = [9, 5, 4, 1, 8];
+function media(notas1, notas2) {
+  return notas1 + notas2 / 2;
+}
+
+// tudo = alunos.concat(media());
+
+console.log(media());
+
+// console.log(notas2);
