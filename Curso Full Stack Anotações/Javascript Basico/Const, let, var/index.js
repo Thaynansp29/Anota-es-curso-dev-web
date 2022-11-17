f740bf2c36a6fd1be8acdbfdaa07e9e28fe549e7
Thaynansp29 <thaynansp29@hotmail.com>
2022-11-17 15:45:52 -0300

    17112022

---
+++ Curso Full Stack Anotações/Javascript Basico/Const, let, var/index.js
@@ -0,0 +1,16 @@
+var numero1 = 4;
+let numero2 = 6;
+const numero3 = 3;
+
+numero3 = "oi";
+{
+  var numero4 = 4;
+  let numero5 = 6;
+  const numero6 = 3;
+}
+console.log(numero1);
+console.log(numero2);
+console.log(numero3);
+console.log(numero4);
+console.log(numero5);
+console.log(numero6);
