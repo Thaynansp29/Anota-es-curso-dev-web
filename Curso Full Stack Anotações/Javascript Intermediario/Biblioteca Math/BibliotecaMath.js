8df5a5a47287d51930025b338acdafe9cecf0660
Thaynansp29 <thaynansp29@hotmail.com>
2023-01-03 14:58:58 -0300

    03012023

---
+++ Curso Full Stack Anotações/Javascript Intermediario/Biblioteca Math/BibliotecaMath.js
@@ -0,0 +1,19 @@
+let pi = Math.PI;
+let b = Math.E;
+let raiz_quadrada = Math.sqrt(16);
+let potencia = Math.pow(5, 2);
+let aleatorio = Math.random(); //um numero aleatorio entre 0.0000000001 a 1.0
+let aleatorio_controlado = Math.random() * 10; //um numero aleatorio entre 0 a 10
+let aleatorio_inteiro = Math.round(Math.random() * 10); //um numero aleatorio entre 0 a 10 so que inteiro
+let arrendondado_para_baixo = Math.floor(5.99); //vai arrendondar para baixo independente do valor do decimal
+let arrendondado_para_cima = Math.ceil(5.01); //vai arrendondar para cima independente do valor do decimal
+
+console.log(pi);
+console.log(b);
+console.log(raiz_quadrada);
+console.log(potencia);
+console.log(aleatorio);
+console.log(aleatorio_controlado);
+console.log(aleatorio_inteiro);
+console.log(arrendondado_para_baixo);
+console.log(arrendondado_para_cima);
